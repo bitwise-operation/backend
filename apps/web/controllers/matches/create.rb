d00636ddc06df1154619089d67f2e772aab33b4a
Bree Stanwyck <bree@bignerdranch.com>
2015-03-05 11:28:55 -0500

    Fake create endpoint for matches

---
+++ apps/web/controllers/matches/create.rb
@@ -0,0 +1,17 @@
+module Web::Controllers::Matches
+  class Create
+    include Lotus::Action
+
+    params do
+      param :scheduled_at, type: DateTime
+      param :opponent_id, type: Integer, presence: true
+    end
+
+    def call(params)
+      match = Match.new(params)
+
+      self.format = :json
+      self.body = MatchSerializer.new(match).to_json
+    end
+  end
+end
