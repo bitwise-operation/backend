53e8d47d7054afe5e0b5be87612c7fbd344d1099
Bree Stanwyck <bree@bignerdranch.com>
2015-03-04 20:55:23 -0500

    Add routes for all planned API routes

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -1,1 +1,26 @@
+# GET /matches/:id
+get '/matches/:id', to: 'matches#show'
+
+# GET /matches # Nice to have
+get '/matches', to: 'matches#index'
+
+# POST /matches
+post '/matches', to: 'matches#create'
+
+# PATCH /matches/:match_id/confirm
+patch '/matches/:match_id/confirm', to: 'matches#confirm'
+
+# DELETE /matches/:match_id
+delete '/matches/:id', to: 'matches#destroy'
+
+# GET /matches/:match_id/scores/:id
+get '/matches/:match_id/scores/:user_id', to: 'scores#show'
+
+# POST /matches/:match_id/scores/:id
+post '/matches/:match_id/scores/:user_id', to: 'scores#create'
+
+# GET /users/:id # Nice to have
+post '/users/:id', to: 'users#show'
+
+# POST /users
 post '/user', to: 'users#create'
