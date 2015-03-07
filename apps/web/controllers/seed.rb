894cf539c19b883af14099e0a32deccec006329b
Kylie Stradley <kylie.stradley@gmail.com>
2015-03-07 13:11:27 -0500

    seed for dates "today"
    seed with scores of 0

--- apps/web/controllers/seed.rb
+++ apps/web/controllers/seed.rb
@@ -8,26 +8,26 @@ class Seed
       creator1 = ScoreRepository.create(
                 Score.new(user_id: UserRepository.create(
                     User.new(name: 'Bree Stanwyck', avatar: 'http://www.bignerdranch.com/img/nerds/headshots/bree-stanwyck.jpg'
-                      )).id, count: 10))
+                      )).id, count: 0))
       opponent1 = ScoreRepository.create(
                  Score.new(user_id: UserRepository.create(
                     User.new(name: 'Angie Terrell', avatar: 'http://www.bignerdranch.com/img/nerds/headshots/angie-terrell.jpg'
-                      )).id, count: 42))
+                      )).id, count: 0))
       match1 = MatchRepository.create(
-              Match.new(creator_id: creator1.id, opponent_id: opponent1.id, state: 'scheduled', scheduled_at: Time.now + 60*60*24))
+              Match.new(creator_id: creator1.id, opponent_id: opponent1.id, state: 'scheduled', scheduled_at: Time.now + 60*60*1))
       creator2 = ScoreRepository.create(
                 Score.new(user_id: UserRepository.create(
                     User.new(name: 'Kristin Marsicano', avatar: 'http://www.bignerdranch.com/img/nerds/headshots/kristin-marsicano.jpg'
-                      )).id, count: 25))
+                      )).id, count: 0))
       opponent2 = ScoreRepository.create(
                  Score.new(user_id: UserRepository.create(
                     User.new(name: 'Kylie Stradley', avatar: 'http://www.bignerdranch.com/img/nerds/headshots/kylie-stradley.jpg'
-                      )).id, count: 3))
+                      )).id, count: 0))
       match2 = MatchRepository.create(
-              Match.new(creator_id: creator2.id, opponent_id: opponent2.id, state: 'scheduled', scheduled_at: Time.now + 60*60*45))
+              Match.new(creator_id: creator2.id, opponent_id: opponent2.id, state: 'scheduled', scheduled_at: Time.now + 60*60*2))
       match3 = MatchRepository.create(
-              Match.new(creator_id: creator2.id, opponent_id: opponent1.id, state: 'scheduled', scheduled_at: Time.now + 60*60*36))
+              Match.new(creator_id: creator2.id, opponent_id: opponent1.id, state: 'scheduled', scheduled_at: Time.now + 60*60*3))
       match4 = MatchRepository.create(
-              Match.new(creator_id: creator1.id, opponent_id: opponent2.id, state: 'scheduled', scheduled_at: Time.now + 60*60*12))
+              Match.new(creator_id: creator1.id, opponent_id: opponent2.id, state: 'scheduled', scheduled_at: Time.now + 60*60*4))
   end
 end
