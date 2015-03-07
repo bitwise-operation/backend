4a36686ebf3d317f9f4299aa007f2816ad0e8cb9
Bree Stanwyck <bree@bignerdranch.com>
2015-03-06 19:17:04 -0500

    "A power so great, it can only be used for Good or Evil!" 		-- Firesign Theatre, "The Giant Rat of Summatra"

--- lib/serializers/user_serializer.rb
+++ lib/serializers/user_serializer.rb
@@ -1,9 +1,17 @@
 require_relative './base_serializer'
 
 class UserSerializer < BaseSerializer
-  attributes :id, :name, :avatar_url
+  attributes :id, :name, :avatar_url, :win_count, :loss_count
 
   def avatar_url
     object.avatar
+  end
+
+  def win_count
+    2
+  end
+
+  def loss_count
+    0
   end
 end
